7a5401f5f14888012134d2b2c6f754dee7360c0c
sophie <sophie1620@gmail.com>
2022-06-02 16:52:43 -0400

    site update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
       <header className="App-header">
         <h1>What to Eat?</h1>
       </header>
+
+      <main>
+        <p>Hello!  Thanks for visiting.  Please stay tuned for an update to the site. Bon appetite!☺️</p>
+      </main>
     </div>
   );
 }
